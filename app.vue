af78800d97c578d883cfaa6db5f6b3bc768fe4e1
Anthony Chu <anthony@anthonychu.ca>
2022-02-08 17:51:16 -0800

    Initial commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div>{{ data }}</div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+const { data, refresh, pending } = await useAsyncData('/api/hello', () => $fetch('/api/hello'))
+</script>
